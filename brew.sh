410bf8d70d4fc68ce9422ff10ba95f14172628f9
James Nettik <james.nettik@gmail.com>
2020-02-24 09:30:56 -0700

    Added list of items for download via Homebrew.

--- brew.sh
+++ brew.sh
@@ -4,9 +4,54 @@ echo "Checking for Homebrew..."
 
 # Check for homebrew and install if necessary.
 if type "brew" > /dev/null; then
-  echo "Homebrew found. Installing dependencies..."
+  echo "Homebrew found."
 else
   echo "Homebrew not found. Installing Homebrew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  echo "Homebrew installed. Installing dependencies"
+  echo "Homebrew installed."
 fi
+
+echo "Updating Homebrw and packages..."
+brew update
+brew upgrade
+BREW_PREFIX=$(brew --prefix)
+
+echo "Tapping Homebrew casks..."
+brew tap caskroom/cask
+brew tap homebrew/cask-fonts
+
+echo "Installing binaries..."
+brew install ansible
+brew install git
+brew install imagemagick --with-webp
+brew install nvm
+brew install php
+brew install the_silver_searcher
+brew install wget
+
+echo "Installing apps..."
+brew cask install 1password
+brew cask install alfred
+brew cask install docker
+brew cask install dropbox
+brew cask install firefox
+brew cask install google-chrome
+brew cask install hyper
+brew cask install microsoft-edge
+brew cask install ngrok
+brew cask install postman
+brew cask install sketch
+brew cask install skype
+brew cask install slack
+brew cask install spotify
+brew cask install tableplus
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install zoomus
+
+echo "Installing fonts..."
+brew cask install font-open-sans
+brew cask install font-ubuntu-mono-derivative-powerline
+
+echo "Cleaning up outdated packages..."
+brew cleanup
