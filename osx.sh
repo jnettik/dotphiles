0b4982f37d57de5e8a2f83028cfbc56e8d8aab91
James Nettik <james.nettik@gmail.com>
2020-03-06 11:18:53 -0700

    Created setup file for testing.

--- osx.sh
+++ osx.sh
@@ -14,10 +14,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "James Nettik's MBP"
-sudo scutil --set HostName "James Nettik's MBP"
+sudo scutil --set ComputerName "James' MBP"
+sudo scutil --set HostName "James' MBP"
 sudo scutil --set LocalHostName "jnettik-mbp"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "James Nettik's MBP"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "James' MBP"
 
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "I looked into the void and the void looked into me..."
