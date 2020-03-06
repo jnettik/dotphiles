6ca1f0239f2daf9457684995057977336a5feb25
James Nettik <james.nettik@gmail.com>
2020-03-06 16:20:23 -0700

    Created folder used for machine organization.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# Create home directory folders.
+for folder in "Sites" "Tools"; do
+  if [[ ! -d ~/${folder} ]]; then
+    mkdir ~/${folder}
+  fi
+done
+
 # Intall Homebrew, binaries, and apps.
 source ./brew.sh
